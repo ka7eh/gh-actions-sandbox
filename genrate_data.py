9d4ac3c6a7066449541cb096dbd1acc11062a2d6
kaveh <kaveh@illinois.edu>
2021-08-11 10:48:14 -0500

    Add data action

---
+++ genrate_data.py
@@ -0,0 +1,20 @@
+import argparse
+from datetime import datetime
+from pathlib import Path
+
+WORK_DIR = Path("./gh-actions-sandbox-submodule")
+
+parser = argparse.ArgumentParser(description="Generate a text file")
+parser.add_argument(dest="author", type=str, help="Creator of the text file")
+
+args = parser.parse_args()
+
+
+def run():
+    time = datetime.now()
+    with open(WORK_DIR / "data" / f"{time}.txt", "w") as f:
+        f.write(f"Generated at {time} by {args.author}.")
+
+
+if __name__ == "__main__":
+    run()
